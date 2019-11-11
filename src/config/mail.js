a18c80a414876064cf318abdbe67064e931dd6c0
VictorVela <victorgvela@hotmail.com>
2019-11-11 20:06:02 -0300

    Configurando Nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'ba80f1e60c41b4',
+    pass: 'd94158466655f0',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com> ',
+  },
+};
---
+++ src/routes.js
@@ -0,0 +1,41 @@
+import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
+
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
+import scheduleController from './app/controllers/ScheduleController';
+import NotificationController from './app/controllers/NotificationController';
+
+import authMiddleware from './app/middleware/auth';
+
+const routes = new Router();
+const upload = multer(multerConfig);
+
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+/*
+somente as requisições abaixo do "authMiddleware" vão
+precisar de token JWT
+ */
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
+
+routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
+routes.delete('/appointments/:id', AppointmentController.delete);
+
+routes.get('/schedule', scheduleController.index);
+
+routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
+
+export default routes;
